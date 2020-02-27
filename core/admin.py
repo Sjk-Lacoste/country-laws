7e2845cb84aaa7be887dc4e149eec3c315ec3755
Sjk-Lacoste <tmohlatlole@gmail.com>
2020-02-27 13:28:10 +0200

    added django-summernote wysiwyg text editor

--- core/admin.py
+++ core/admin.py
@@ -1,20 +1,27 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import (
     Continent,
     Law,
     Country
 )
 
-class ContinentAdmin(admin.ModelAdmin):
+class ContinentAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
     list_display = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
 
 
-class LawAdmin(admin.ModelAdmin):
+class LawAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
     list_display = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
 
 
-class CountryAdmin(admin.ModelAdmin):
+class CountryAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
     list_display = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
 
 # Register your models here.
 admin.site.register(Continent, ContinentAdmin)
--- country_laws/settings.py
+++ country_laws/settings.py
@@ -36,7 +36,11 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
+    'django_summernote',
+    
     'core',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,5 +127,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+SUMMERNOTE_THEME = 'bs4'
+
 django_heroku.settings(locals())
