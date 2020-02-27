94e0506d2454208b2aee32d49d7a07ce5e24d816
Sjk-Lacoste <tmohlatlole@gmail.com>
2020-02-27 14:16:02 +0200

    Changed database configurations

--- country_laws/settings.py
+++ country_laws/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'country_laws.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'country_laws',
+        'USER': 'sjk_lacoste',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
