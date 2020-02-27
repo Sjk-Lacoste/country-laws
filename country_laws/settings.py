6cf3ab5911d00a3b084798c942c9f10276742304
Sjk-Lacoste <tmohlatlole@gmail.com>
2020-02-27 15:50:06 +0200

    Created migrations

--- country_laws/settings.py
+++ country_laws/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'country_laws.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'country_laws',
-        'USER': 'sjk_lacoste',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
