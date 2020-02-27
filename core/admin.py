5a6fcbf265d0500e1a424c6db93b7587e2cdbb54
Sjk-Lacoste <tmohlatlole@gmail.com>
2020-02-27 10:29:54 +0200

    Added core models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (
+    Continent,
+    Law,
+    Country
+)
+
 
 # Register your models here.
+admin.sites.register(Continent)
+admin.sites.register(Law)
+admin.sites.register(Country)
