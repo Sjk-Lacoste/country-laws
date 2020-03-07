055ac02b2507bbfc3e4bb9aabcea015445579e46
Sjk-Lacoste <tmohlatlole@gmail.com>
2020-03-07 17:29:39 +0200

    Styling country laws frontend

--- core/views.py
+++ core/views.py
@@ -20,9 +20,12 @@ def get_started(request):
 
         from_country = request.POST.get('from_country', '')
         to_country = request.POST.get('to_country', '')
-        
+        country = Country.objects.filter(pk=to_country)
+        country_slug = ''
+        for c in country:
+            country_slug = c.slug
         if from_country and to_country:
-            return HttpResponseRedirect('/country-related-laws/')
+            return HttpResponseRedirect(f'/country-related-laws/{country_slug}')
 
     context = {
         'title': 'Getting Started',
@@ -36,7 +39,9 @@ def country_laws(request, slug):
     country = Country.objects.get(slug=slug)
     laws = get_laws_in_country(country.id)
     context = {
-        'title': country.name
+        'title': country.name,
+        'laws': laws,
+        'country': country
     }
 
     return render(request, 'core/country_laws.html', context)
